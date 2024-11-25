ba9a8aa1f9c90c10ffeaa70bf3067d6893cec41c
abizec-p <n01674616@humbermail.ca>
2024-11-25 17:33:40 -0500

    more of em nov

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,46 @@
 import Navigation from "./Navigation";
 import bottleAD from "../assets/images/waterBottle.png";
+import portfolio from "../assets/images/website-design.png"
 import advert from "../assets/images/advert.png";
+import yak from "../assets/images/yak.png";
+import logodesign from "../assets/images/logoforaj.png";
 import "./portfolio.css";
 import { Link } from "react-router-dom";
 
 export default function Portfolio() {
+  
   const worksData = [
     {
-      category:"advertisement",
+      category: "advertisement",
       title: "Bottle advertisement",
       description:
         " This is a Bottle advertisement i created when learning photoshop.I used photoshop for creating this.",
       image: bottleAD,
     },
     {
-      category:"advertisement",
+      category: "advertisement",
       title: "advertisement",
       description:
         " This is a Beer can advertisement i created for college project.I used photoshop, Illustration and Indesign for creating this.",
       image: advert,
+    },
+    {
+      category: "logo",
+      title: "Portfolio website",
+      description: " This is a portfolio website. i created for college.",
+      image: logodesign,
+    },
+    {
+      category: "webDesign",
+      title: "Portfolio website",
+      description: " This is a portfolio website. i created for college.",
+      image: portfolio,
+    },
+    {
+      category: "illustration",
+      title: "Portfolio website",
+      description: " This is a portfolio website. i created for college.",
+      image: yak,
     },
   ];
 
@@ -29,5 +51,19 @@ export default function Portfolio() {
         <Link to="/webdesign">Webdesign</Link>
         <Link to="/advertisement">Advertisement</Link>
         <Link to="/Illustration">Illustration</Link>
+        <Link to="/logo">Logo</Link>
       </div>
-      <div className="port
+      <div className="portfolio-works-section">
+        {worksData.map((data, index) => (
+          <div className="work-card" key={index}>
+            <img src={data.image} alt="" />
+            <div className="portfolio-works-details">
+              <h1>{data.title}</h1>
+              <p>{data.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
