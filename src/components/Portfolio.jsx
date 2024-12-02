c85bdc89f0ad36928b5b54b34e1ff6a625a5b371
abizec-p <n01674616@humbermail.ca>
2024-12-02 03:00:17 -0500

    framer

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,17 +11,19 @@ import { Link } from "react-router-dom";
 import { Route, Router, Routes } from "react-router-dom";
 import Illustration from "./category/illustration";
 import { useState } from "react";
-
-
-
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function Portfolio() {
   const [activeCategory, setActiveCategory] = useState("all");
-
+  const [selectedCard, setSelectedCard] = useState(null);
 
   const isActive = null;
   const worksData = [
     {
+      id: 1,
+      Client: "college project",
+      skills: "Photoshop, illustrator",
       category: "advertisement",
       title: "Bottle advertisement",
       description:
@@ -29,25 +31,40 @@ export default function Portfolio() {
       image: bottleAD,
     },
     {
-      category: "advertisement",
-      title: "advertisement",
+      id: 2,
+      Client: "college project",
+      skills: "Photoshop, illustrator",
+
+      category: "Advertisement",
+      title: "Advertisement",
       description:
         " This is a Beer can advertisement i created for college project.I used photoshop, Illustration and Indesign for creating this.",
       image: advert,
     },
     {
+      id: 3,
+      Client: "college project",
+      skills: "Photoshop, illustrator",
+
       category: "logo",
-      title: "Portfolio website",
+      title: "Logo",
       description: " This is a portfolio website. i created for college.",
       image: logodesign,
     },
     {
+      id: 4,
+      Client: "college project",
+      skills: "Photoshop, illustrator",
+
       category: "webDesign",
       title: "Portfolio website",
       description: " This is a portfolio website. i created for college.",
       image: portfolio,
     },
     {
+      id: 5,
+      Client: "college project",
+      skills: "Photoshop, illustrator",
       category: "illustration",
       title: "Portfolio website",
       description: " This is a portfolio website. i created for college.",
@@ -55,49 +72,86 @@ export default function Portfolio() {
     },
   ];
   const filteredWorks = worksData.filter(
-    (work) =>
-      activeCategory === "all" || work.category === activeCategory
+    (work) => activeCategory === "all" || work.category === activeCategory
   );
+  const handleCardClick = (data) => {
+    setSelectedCard(data);
+  };
+  const closePopUp = () => {
+    setSelectedCard(null);
+  };
 
   return (
     <section className="portfolio">
       <Navigation />
 
       <div className="category">
-        <Link onClick={()=>setActiveCategory("all")} >All</Link>
-        <Link
-          onClick={() => setActiveCategory("webDesign")}
-        >
-          Webdesign
-        </Link>
-        <Link
-          onClick={() => setActiveCategory("advertisement")}
-        >
+        <Link onClick={() => setActiveCategory("all")}>All</Link>
+        <Link onClick={() => setActiveCategory("webDesign")}>Webdesign</Link>
+        <Link onClick={() => setActiveCategory("advertisement")}>
           Advertisement
         </Link>
-        <Link
-          onClick={() => setActiveCategory("Illustration")}
-        >
+        <Link onClick={() => setActiveCategory("Illustration")}>
           Illustration
         </Link>
-        <Link
-          onClick={() => setActiveCategory("logo")}
-        >
-          Logo
-        </Link>
+        <Link onClick={() => setActiveCategory("logo")}>Logo</Link>
       </div>
 
       <div className="portfolio-works-section">
         {filteredWorks.map((data, index) => (
-          <div className="work-card" key={index}>
+          <motion.div
+            onClick={() => handleCardClick(data)}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="work-card"
+            key={data.id}
+          >
             <img src={data.image} alt="" />
             <div className="portfolio-works-details">
               <h1>{data.title}</h1>
               <p>{data.description}</p>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
+
+      {selectedCard && (
+        <motion.div
+          className="popup-overlay"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          onClick={closePopUp}
+        >
+          <motion.div
+            className="popup-content"
+            initial={{ scale: 0.5 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0 }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div>
+              <img className="popup-image" src={selectedCard.image}></img>
+              <button onClick={closePopUp}>X</button>
+            </div>
+            <div className="popup-details">
+              <h2>{selectedCard.title}</h2>
+              <p>{selectedCard.description}</p>
+              <br />
+              <h4>Client:</h4>
+              <p>{selectedCard.Client}</p>
+              <br />
+              <h4>Type of work:</h4>
+              <p>{selectedCard.category}</p>
+              <br />
+              <span>
+                <h4>skills:</h4>
+              </span>{" "}
+              <p className="card-skills">{selectedCard.skills}</p>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
     </section>
   );
 }
---
+++ src/components/workdetails.jsx
@@ -0,0 +1,6 @@
+export default function WorkDetail() {
+    return (
+        <>
+            <h1>hi</h1></>
+    )
+}
